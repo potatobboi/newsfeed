1ba10b578d2952ffe28be496adcacf4503e65456
LeeJaeHyung <leejh7623@gmail.com>
2024-02-07 19:54:54 +0900

    주석 추가

--- src/main/java/com/sparta/newsfeed/service/CommentService.java
+++ src/main/java/com/sparta/newsfeed/service/CommentService.java
@@ -20,33 +20,33 @@ public class CommentService {
     }
 
 
-    public List<CommentResponseDto> getComments(Long postId) {
+    public List<CommentResponseDto> getComments(Long postId) { // 게시글 id를 기준으로 속해있는 모든 댓글을 가져옴
         return commentRepository.findByPostIdOrderByCreatedAtAsc(postId).stream().map(CommentResponseDto::new).toList();
     }
 
-    public CommentResponseDto createComment(CommentRequestDto requestDto) {
+    public CommentResponseDto createComment(CommentRequestDto requestDto) { // 댓글 생성
         Comment comment = new Comment(requestDto);
         return new CommentResponseDto(commentRepository.save(comment));
     }
 
     @Transactional
-    public Long updateComment(Long id, CommentRequestDto requestDto) {
-        Comment comment = findComment(id);
+    public Long updateComment(Long id, CommentRequestDto requestDto) { //댓글 id를 기준으로 댓글 update
+        Comment comment = findyComment(id);
         comment.update(requestDto);
         return id;
     }
 
-    public Long deleteComment(Long id) {
-        Comment comment = findComment(id);
+    public Long deleteComment(Long id) { //댓글 id를 기준으로 댓글삭제
+        Comment comment = findyComment(id);
         commentRepository.delete(comment);
         return id;
     }
 
-    public Comment findComment(Long id){
-        return commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 메모는 존재하지 않습니다."));
+    public Comment findyComment(Long id){// id를 기준으로 댓글 찾아옴
+        return commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 댓글은 존재하지 않습니다."));
     }
 
-    public boolean deleteCommentByPost_id(Long postId){
+    public boolean deleteCommentByPostId(Long postId){ //post의 id를 기준으로 모든 댓글 삭제
         commentRepository.deleteByPostId(postId);
         return false;
     }
