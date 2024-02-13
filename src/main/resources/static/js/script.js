e1ea5ef1788eb652323a2d3b4a64c74556564a84
kudongku <kudongku@naver.com>
2024-02-13 21:28:06 +0900

    feat(front): 전체조회, 선택조회 가능

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,93 @@
-$(document).ready(function () {
-    $(`#showForEdit`).hide();
-    $(`#showForEditPassword`).hide();
-})
+function getPosts() {
+    // AJAX를 사용하여 서버에 POST 요청 보내기
+    $.ajax({
+        type: 'GET',
+        url: '/api/posts',
+        contentType: 'application/json',
+        success: function (response) {
+            for (let i = 0; i < response.length; i++) {
+                addHtml(response[i].id, response[i].username, response[i].title);
+            }
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.responseJSON.message;
+            alert(errorMessage);
+        }
+    });
+}
+
+function addHtml(id, username, title) {
+    let tempHtml =
+        `<div class="card" onclick="showDetails('${id}')"/>
+            <p>${username}</p>
+            <h1>${title}</h1>
+        </div>`;
+    $('#cards-box').append(tempHtml);
+}
+
+function showDetails(id) {
+    $.ajax({
+        type: 'GET',
+        url: `/api/posts/postId/${id}`,
+        contentType: 'application/json',
+        success: function (response) {
+            let id = response['id'];
+            let modifiedDate = response['modifiedDate'];
+            let title = response['title'];
+            let username = response['username']
+            let content = response['content'];
+            addHtmlDetails(id, modifiedDate, title, username, content);
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.responseJSON.message;
+            alert(errorMessage);
+        }
+    });
+}
+
+function addHtmlDetails(id, modifiedDate, title, username, content) {
+    let tempHtml =
+        `<div class="card">
+            <p>${username}</p>
+            <p>${modifiedDate}</p>
+            <h1>${title}</h1>
+            <h2>${content}</h2>
+            <button id="${id}-edit" onclick="editPost('${id}')">수정</button>
+            <button id="${id}-submit" onclick="submitEdit('${id}')">완료</button>
+            <button id="${id}-delete" onclick="deletePost('${id}')">삭제</button>
+        </div>`;
+    $('#detail-box').append(tempHtml);
+}
+function editPost() {
+    // let username = $('#username').val();
+    // let password = $('#password').val();
+    //
+    // // 전송할 데이터 객체 생성
+    // let data = {
+    //     'username': username,
+    //     'password': password,
+    // };
+    //
+    // // AJAX를 사용하여 서버에 POST 요청 보내기
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/api/users/login',
+    //     contentType: 'application/json',
+    //     data: JSON.stringify(data),
+    //     success: function () {
+    //         window.location.href = "http://localhost:8080/";
+    //     },
+    //     error: function (xhr, status, error) {
+    //         var errorMessage = xhr.responseJSON.message;
+    //         alert(errorMessage);
+    //         window.location.reload();
+    //
+    //     }
+    // });
+}
+
+
+
 function signup() {
     let username = $('#username').val();
     let password = $('#password').val();
@@ -92,9 +178,11 @@ function sendVerifyEmail() {
 function editInfo() {
     $(`#showForEdit`).show();
 }
+
 function editPassword() {
     $(`#showForEditPassword`).show();
 }
+
 function submitinfo() {
     let editinfo = $('#editinfo').val();
 
