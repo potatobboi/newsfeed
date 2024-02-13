41bad8933d1f38c2746e13b4944cc78d9cd7d49f
kudongku <kudongku@naver.com>
2024-02-13 20:03:50 +0900

    feat: security 활성화

--- src/main/java/com/sparta/newsfeed/NewsfeedApplication.java
+++ src/main/java/com/sparta/newsfeed/NewsfeedApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class NewsfeedApplication {
 
     public static void main(String[] args) {
